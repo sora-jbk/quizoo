a8e4bd8a6121e77a4b58e074abf6d668e3b3ef1b
sora-jbk <aj22304027@ga.ttc.ac.jp>
2023-11-07 11:44:51 +0900

    ApplicationControllerインターフェイスを作成

--- quizoo/src/main/java/frame/ApplicationController.java
+++ quizoo/src/main/java/frame/ApplicationController.java
@@ -1,1 +1,9 @@
-public interface ApplicationController {} 
+import org.apache.tomcat.util.http.fileupload.RequestContext;
+
+import frame.context.ResponseContext;
+
+public interface ApplicationController {
+	public RequestContext getRequest(Object request);
+	public ResponseContext handleRequest(RequestContext request);
+	public void handleResponse(RequestContext request,ResponseContext response);
+} 
