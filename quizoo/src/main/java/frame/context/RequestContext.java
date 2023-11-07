c9c2209bb08e3435ab322d6f53f7b84e7a319831
sora-jbk <aj22304027@ga.ttc.ac.jp>
2023-11-07 11:36:07 +0900

    RequestContextインターフェイスを作成

--- quizoo/src/main/java/frame/context/RequestContext.java
+++ quizoo/src/main/java/frame/context/RequestContext.java
@@ -1,1 +1,6 @@
-public interface RequestContext {} 
+public interface RequestContext {
+	public void setAttrubute(String key,Object value);
+	public String[] getParameter(String key);
+	public void setId(String id);
+	public String getId();
+} 
