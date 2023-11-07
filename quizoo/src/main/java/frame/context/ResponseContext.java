7a6397c85b1bbe09fccddc6f21000a3409cfe9c2
sora-jbk <aj22304027@ga.ttc.ac.jp>
2023-11-07 11:37:43 +0900

    ResponseContextインターフェイスを作成

--- quizoo/src/main/java/frame/context/ResponseContext.java
+++ quizoo/src/main/java/frame/context/ResponseContext.java
@@ -1,1 +1,8 @@
-public interface ResponseContext {} 
+import java.io.PrintWriter;
+
+import org.apache.tomcat.util.http.fileupload.RequestContext;
+
+public interface ResponseContext {
+	public void forward(String uri,RequestContext req);
+	public PrintWriter getWriter();
+} 
