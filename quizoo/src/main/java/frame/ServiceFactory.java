f12ff3a32a34e2d64edbc2c2d30782a12a17b1bb
sora-jbk <aj22304027@ga.ttc.ac.jp>
2023-11-09 16:13:07 +0900

    いろいろ修正

--- quizoo/src/main/java/frame/ServiceFactory.java
+++ quizoo/src/main/java/frame/ServiceFactory.java
@@ -3,8 +3,8 @@ package frame;
 import frame.context.RequestContext;
 
 public abstract class ServiceFactory {
-	public static Service getService(RequestContext req) {
-		String target = req.getTarget();
+	public static Service getService(RequestContext req) throws ClassNotFoundException, InstantiationException, IllegalAccessException {
+		String target = req.getRequestTarget();
 		
 		Class c = Class.forName(target);
 		
--- quizoo/src/main/java/web/WebApplicationController.java
+++ quizoo/src/main/java/web/WebApplicationController.java
@@ -1,5 +1,9 @@
 package web;
 
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
@@ -10,19 +14,33 @@ import web.context.HttpRequestContext;
 import web.context.HttpResponseContext;
 
 public class WebApplicationController implements ApplicationController{
+	
+	HttpServletRequest req = null;
+	HttpServletResponse res = null;
 
 	@Override
 	public RequestContext getRequest(Object request) {
+		this.req = (HttpServletRequest)request;
 		return new HttpRequestContext((HttpServletRequest)request);
 	}
 
 	@Override
 	public ResponseContext handleRequest(Object response) {
+		this.res = (HttpServletResponse)response;
 		return new HttpResponseContext((HttpServletResponse)response);
 	}
 
 	@Override
 	public void handleResponse(RequestContext request, ResponseContext response) {
-		
+		String target = request.getRequestTarget();
+		if(target != null) {
+			RequestDispatcher disp = req.getRequestDispatcher("target");
+			try {
+				disp.forward(req, res);
+			} catch (ServletException | IOException e) {
+				// TODO 自動生成された catch ブロック
+				e.printStackTrace();
+			}
+		}
 	}
 } 
