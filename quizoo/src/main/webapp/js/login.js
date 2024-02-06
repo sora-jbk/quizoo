37a987f6d9d312294d79fdaddc95e0340bad22b9
wataru-51 <kw22304025@ga.ttc.ac.jp>
2024-02-06 12:15:38 +0900

    signupのとこだけやった。半角の英数字と記号以外が入ると、アラートが出るようにしたが、文字を空にしたときもアラートが出てしまう。

--- quizoo/src/main/webapp/js/login.js
+++ quizoo/src/main/webapp/js/login.js
@@ -12,3 +12,23 @@ window.addEventListener('load', function() {
 
 	}
 });
+
+
+var inputId = document.getElementById("register-userid");
+inputId.addEventListener('input', function() {
+	var inputValue = document.getElementById("register-userid").value;
+	var regex = /^[a-zA-Z0-9!@#$%^&*()_+{}\[\]:;<>,.?~\\/-]+$/;
+
+	if (!regex.test(inputValue)) {
+
+		alert("半角の英数字か記号でお願いします");
+
+		document.getElementById("register-userid").value=inputValue.slice(0, -1);
+		
+		return false;
+	}
+
+	
+
+	return true;
+})
