22e98865ef798b72f2dc4261350094aba8c3ffdc
wataru-51 <kw22304025@ga.ttc.ac.jp>
2024-02-06 16:06:27 +0900

    アラートじゃなくした

--- quizoo/src/main/webapp/js/login.js
+++ quizoo/src/main/webapp/js/login.js
@@ -13,35 +13,45 @@ window.addEventListener('load', function() {
 	}
 });
 
+var loginInputId = document.getElementById("login-username");
+loginInputId.addEventListener('input', function(event) {
 
-var registInputId = document.getElementById("register-userid");
-registInputId.addEventListener('input', function() {
-	var inputValue = document.getElementById("register-userid").value;
-	var regex = /^[a-zA-Z0-9!@#$%^&*()_+{}\[\]:;<>,.?~\\/-]+$/;
+	// 入力された文字列を取得
+	var inputValue = event.target.value;
 
-	if (!regex.test(inputValue)) {
+	// 英数字と記号以外の文字を無視
+	var sanitizedValue = inputValue.replace(/[^a-zA-Z0-9!@#$%^&*()_+{}\[\]:;<>,.?~\\/-]/g, '');
 
-		alert("半角の英数字か記号でお願いします");
-
-		document.getElementById("register-userid").value=inputValue.slice(0, -1);
-		
-		return false;
+	if (inputValue !== sanitizedValue) {
+		// 無視する文字が含まれている場合はエラーメッセージを表示
+		document.getElementById("login-errorMessage").textContent = "英数字および記号以外は使えません";
+	} else {
+		// エラーメッセージをクリア
+		document.getElementById("login-errorMessage").textContent = "";
 	}
-	return true;
+
+	// 入力を修正
+	event.target.value = sanitizedValue;
 });
 
-var loginInputId = document.getElementById("login-username");
-loginInputId.addEventListener('input', function() {
-	var inputValue = document.getElementById("login-username").value;
-	var regex = /^[a-zA-Z0-9!@#$%^&*()_+{}\[\]:;<>,.?~\\/-]+$/;
 
-	if (!regex.test(inputValue)) {
+var registInputId = document.getElementById("register-userid");
+registInputId.addEventListener('input', function(event) {
 
-		alert("半角の英数字か記号でお願いします");
+	// 入力された文字列を取得
+	var inputValue = event.target.value;
 
-		document.getElementById("login-username").value=inputValue.slice(0, -1);
-		
-		return false;
+	// 英数字と記号以外の文字を無視
+	var sanitizedValue = inputValue.replace(/[^a-zA-Z0-9!@#$%^&*()_+{}\[\]:;<>,.?~\\/-]/g, '');
+
+	if (inputValue !== sanitizedValue) {
+		// 無視する文字が含まれている場合はエラーメッセージを表示
+		document.getElementById("signup-errorMessage").textContent = "英数字および記号以外は使えません";
+	} else {
+		// エラーメッセージをクリア
+		document.getElementById("signup-errorMessage").textContent = "";
 	}
-	return true;
+
+	// 入力を修正
+	event.target.value = sanitizedValue;
 });
