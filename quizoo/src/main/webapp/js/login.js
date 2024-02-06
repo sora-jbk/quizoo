3030b6e688b428daa48d77f615f39068671f92b1
wataru-51 <kw22304025@ga.ttc.ac.jp>
2024-02-06 12:21:30 +0900

    ログインにも半角英数字にした

--- quizoo/src/main/webapp/js/login.js
+++ quizoo/src/main/webapp/js/login.js
@@ -14,8 +14,8 @@ window.addEventListener('load', function() {
 });
 
 
-var inputId = document.getElementById("register-userid");
-inputId.addEventListener('input', function() {
+var registInputId = document.getElementById("register-userid");
+registInputId.addEventListener('input', function() {
 	var inputValue = document.getElementById("register-userid").value;
 	var regex = /^[a-zA-Z0-9!@#$%^&*()_+{}\[\]:;<>,.?~\\/-]+$/;
 
@@ -27,8 +27,21 @@ inputId.addEventListener('input', function() {
 		
 		return false;
 	}
-
-	
-
 	return true;
-})
+});
+
+var loginInputId = document.getElementById("login-username");
+loginInputId.addEventListener('input', function() {
+	var inputValue = document.getElementById("login-username").value;
+	var regex = /^[a-zA-Z0-9!@#$%^&*()_+{}\[\]:;<>,.?~\\/-]+$/;
+
+	if (!regex.test(inputValue)) {
+
+		alert("半角の英数字か記号でお願いします");
+
+		document.getElementById("login-username").value=inputValue.slice(0, -1);
+		
+		return false;
+	}
+	return true;
+});
