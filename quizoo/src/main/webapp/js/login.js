13894973e4674e2a3e1a8b04694348d7fce85d9a
wataru-51 <kw22304025@ga.ttc.ac.jp>
2024-02-15 09:18:02 +0900

    いらないところを削除

--- quizoo/src/main/webapp/js/login.js
+++ quizoo/src/main/webapp/js/login.js
@@ -1,16 +1,15 @@
 /**
  * 
  */
-
 window.addEventListener('load', function() {
 	var queryString = null;
 	queryString = location.search;
 	//console.log(queryString);
-
 	if (queryString == '?faild') {
 		document.getElementById('miss').classList.remove('none');
 	}
 });
+
 
  //login
 var loginInputId = document.getElementById("login-username");
@@ -27,40 +26,44 @@ registInputId.addEventListener('input', function(event) {
 });
 
 
+
 function change(event) {
 	var regexp = /[^a-zA-Z0-9!@#$%^&*()_+{}\[\]:;<>,.?~\\/-]/;
-
 	if (event.isComposing || event.key === 'Process') {
 		// IME入力中
 
 		event.target.disabled = true;
 		event.target.disabled = false;
-		
-		setTimeout(() => {
-			event.target.focus();
-		}, 5);
-		event.target.value = event.target.value.replace(regexp,"");
-		console.log("IME中"+event.target.value);
 
-		
+		//タイムラグつけないとforcsできない
+		sleep(event, 10);
+		//IME中に入った全角を消す
+		event.target.value = event.target.value.replace(regexp,"");
+
+		//エラーメッセージ表示
 		document.getElementById(message).style.display = "inline";
 	} else {
 		// IME入力中でない
 		event.target.disabled = true;
 		event.target.disabled = false;
-		
+
+		//タイムラグつけないとforcsできない
 		setTimeout(() => {
 			event.target.focus();
 		}, 5);
-		
-		inputValue = event.target.value;
-		console.log("IME外:"+ inputValue);
-		if(regexp.test(inputValue)) {
-			console.log("非正規：");
-			event.target.value = inputValue.replace(regexp,"");
-		}
+		console.log("たまに打てる");
+		//おそらく、forcsまでに入力された全角文字が入るから消す
+		event.target.value = event.target.value.replace(regexp,"");
 
 		// エラーメッセージをクリア
 		document.getElementById(message).style.display = "none";
 	}
   }
+function sleep(event,ms) {
+	setTimeout(() => {
+		event.target.focus();
+	}, 5);
+	console.log("ss");
+	return new Promise(resolve => setTimeout(resolve, ms));
+};
+
